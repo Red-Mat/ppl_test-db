96fa775431be245c7dbd06e1bca16c0065456d45
Red-Mat <jesus.ing.rodriguez@gmail.com>
2020-03-22 11:45:04 -0300

    dataurl

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     try:
-        dato = environ[""]
+        dato = environ["DATABASE_URL"]
     except:
          return "hola Wachin"
     else:
