720f74e246a73edcf43702c9266d85c3de0a9188
Red-Mat <jesus.ing.rodriguez@gmail.com>
2020-03-22 02:25:50 -0300

    Init-testing

---
+++ app.py
@@ -0,0 +1,13 @@
+from os import environ
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return environ["HOME"]
+
+
+if __name__ == '__main__':
+  app.run(debug= True)
